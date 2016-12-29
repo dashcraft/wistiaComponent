178131cf327bec625c8af51f07adde08398d0f58
Daniel Ashcraft <ashcraftbookkeeping@gmail.com>
2016-12-28 18:16:24 -0600

    remove console logs for debugging
    Remove console logs

--- wistia_component.js
+++ wistia_component.js
@@ -17,14 +17,9 @@ angular.module('portfolioApp')
         api_password:wistiapass
       },
       add: function(e,data){
-        console.log(data);
-        console.log(e);
         data.submit();
       },
       progressall: function (e, data) {
-        console.log(data);
-        console.log(e);
-
         var progress = parseInt(data.loaded / data.total * 100, 10);
         $('#progress .bar').css(
             'width',
@@ -34,8 +29,6 @@ angular.module('portfolioApp')
       },
       done: function(e,data){
         hash = 'http://fast.wistia.net/embed/iframe/' + data.result.hashed_id;
-        console.log('hash', hash);
-        console.log('full url','http://fast.wistia.net/embed/iframe/' + hash);
         $('#fileupload').before('<iframe src=\"'+hash+"\" class=\"wistia_embed\" allowtransparency=\"true\" frameborder=\"0\" scrolling=\"no\" width=\"640\" height=\"360\" name=\"wistia_embed\"></iframe>");
       }
     });
